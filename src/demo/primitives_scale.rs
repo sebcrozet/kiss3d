ede5ad19f4d0253de9adeb03717b90419a13bfb1
Sébastien Crozet <developer@crozet.re>
2013-06-30 20:50:20 +0000

    Objects transform now uses higher precision floats (on the cpu side).

--- src/demo/primitives_scale.rs
+++ src/demo/primitives_scale.rs
@@ -17,29 +17,29 @@ fn main()
       let dim: f32 = random();
       let dim2 = dim / 2.0;
 
-      let offset = i as f32 * 1.0 - 5.0;
+      let offset = i as f64 * 1.0 - 5.0;
 
       w.add_cube(dim2, dim2, dim2).set_color(random(), random(), random())
                                   .transformation()
-                                  .translate_by(&Vec3::new::<f32>([0.0, 0.5, offset]));
+                                  .translate_by(&Vec3::new([0.0, 0.5, offset]));
 
       w.add_sphere(dim2).set_color(random(), random(), random())
                         .transformation()
-                        .translate_by(&Vec3::new::<f32>([0.0, -0.5, offset]));
+                        .translate_by(&Vec3::new([0.0, -0.5, offset]));
 
       w.add_cone(dim, dim2).set_color(random(), random(), random())
                            .transformation()
-                           .translate_by(&Vec3::new::<f32>([0.0, 1.5, offset]));
+                           .translate_by(&Vec3::new([0.0, 1.5, offset]));
 
       w.add_cylinder(dim, dim2).set_color(random(), random(), random())
                                .transformation()
-                               .translate_by(&Vec3::new::<f32>([0.0, -1.5, offset]));
+                               .translate_by(&Vec3::new([0.0, -1.5, offset]));
     }
 
     do w.set_loop_callback |w|
     {
       for w.objects().iter().advance |o|
-      { rotate_wrt_center(o.transformation(), &Vec3::new::<f32>([0.0, 0.014, 0.0])); }
+      { rotate_wrt_center(o.transformation(), &Vec3::new([0.0f64, 0.014, 0.0])); }
     };
 
     w.set_light(StickToCamera);
--- src/object.rs
+++ src/object.rs
@@ -7,13 +7,13 @@ use glcore::functions::GL_VERSION_1_1::*;
 use glcore::functions::GL_VERSION_2_0::*;
 use glcore::consts::GL_VERSION_1_1::*;
 use nalgebra::traits::homogeneous::ToHomogeneous;
-use nalgebra::traits::transpose::Transpose;
+use nalgebra::traits::indexable::Indexable;
 use nalgebra::adaptors::transform::Transform;
 use nalgebra::adaptors::rotmat::Rotmat;
 use nalgebra::mat::{Mat3, Mat4};
 use nalgebra::vec::Vec3;
 
-type Transform3d = Transform<Rotmat<Mat3<GLfloat>>, Vec3<GLfloat>>;
+type Transform3d = Transform<Rotmat<Mat3<f64>>, Vec3<f64>>;
 type Scale3d     = Mat3<GLfloat>;
 
 pub struct GeometryIndices
@@ -69,22 +69,54 @@ impl Object
                 scale_location:            i32,
                 normal_transform_location: i32)
   {
-    let mut formated_transform:  Mat4<GLfloat> = self.transform.to_homogeneous();
-    let mut formated_ntransform: Mat3<GLfloat> = self.transform.submat().submat();
+    let formated_transform:  Mat4<f64> = self.transform.to_homogeneous();
+    let formated_ntransform: Mat3<f64> = self.transform.submat().submat();
 
-    formated_transform.transpose();
-    formated_ntransform.transpose();
+    // we convert the matrix elements and do the transposition at the same time
+    let transform_glf = Mat4::new ([
+      formated_transform.at((0, 0)) as GLfloat,
+      formated_transform.at((1, 0)) as GLfloat,
+      formated_transform.at((2, 0)) as GLfloat,
+      formated_transform.at((3, 0)) as GLfloat,
+
+      formated_transform.at((0, 1)) as GLfloat,
+      formated_transform.at((1, 1)) as GLfloat,
+      formated_transform.at((2, 1)) as GLfloat,
+      formated_transform.at((3, 1)) as GLfloat,
+
+      formated_transform.at((0, 2)) as GLfloat,
+      formated_transform.at((1, 2)) as GLfloat,
+      formated_transform.at((2, 2)) as GLfloat,
+      formated_transform.at((3, 2)) as GLfloat,
+
+      formated_transform.at((0, 3)) as GLfloat,
+      formated_transform.at((1, 3)) as GLfloat,
+      formated_transform.at((2, 3)) as GLfloat,
+      formated_transform.at((3, 3)) as GLfloat,
+    ]);
+
+    let ntransform_glf = Mat3::new ([
+      formated_transform.at((0, 0)) as GLfloat,
+      formated_transform.at((1, 0)) as GLfloat,
+      formated_transform.at((2, 0)) as GLfloat,
+      formated_transform.at((0, 1)) as GLfloat,
+      formated_transform.at((1, 1)) as GLfloat,
+      formated_transform.at((2, 1)) as GLfloat,
+      formated_transform.at((0, 2)) as GLfloat,
+      formated_transform.at((1, 2)) as GLfloat,
+      formated_transform.at((2, 2)) as GLfloat,
+    ]);
 
     unsafe {
       glUniformMatrix4fv(transform_location,
                          1,
                          GL_FALSE,
-                         ptr::to_unsafe_ptr(&formated_transform.mij[0]));
+                         ptr::to_unsafe_ptr(&transform_glf.mij[0]));
 
       glUniformMatrix3fv(normal_transform_location,
                          1,
                          GL_FALSE,
-                         ptr::to_unsafe_ptr(&formated_ntransform.mij[0]));
+                         ptr::to_unsafe_ptr(&ntransform_glf.mij[0]));
 
       glUniformMatrix3fv(scale_location,
                          1,
