aa67820b7505bc4adf3f3e95ba3115df7971d587
Sébastien Crozet <developer@crozet.re>
2013-10-11 01:15:31 +0200

    Allow default keyboard handling on the stereo demo.

--- examples/stereo.rs
+++ examples/stereo.rs
@@ -73,7 +73,7 @@ fn main() {
                                 false
                             },
                             _ => {
-                                false
+                                true
                             },
                         }
                     }
