26421167057806f26e06dec52646b93f5eb75a77
Sébastien Crozet <developer@crozet.re>
2014-06-09 22:15:35 +0200

    Update the documentation.

--- doc/implementors/nalgebra/traits/geometry/trait.Translation.js
+++ doc/implementors/nalgebra/traits/geometry/trait.Translation.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['kiss3d'] = ["impl <a class='trait' href='http://www.rust-ci.org/sebcrozet/nalgebra/doc/nalgebra/traits/geometry/trait.Translation.html' title='nalgebra::traits::geometry::Translation'>Translation</a>&lt;<a class='struct' href='http://www.rust-ci.org/sebcrozet/nalgebra/doc/nalgebra/structs/vec/struct.Vec3.html' title='nalgebra::structs::vec::Vec3'>Vec3</a>&lt;<a href='http://doc.rust-lang.org/std/primitive.f32.html'>f32</a>&gt;&gt; for <a class='struct' href='kiss3d/camera/struct.FirstPerson.html' title='kiss3d::camera::FirstPerson'>FirstPerson</a>",];
+implementors['kiss3d'] = ["impl <a class='trait' href='http://nalgebra.org/doc/nalgebra/traits/geometry/trait.Translation.html' title='nalgebra::traits::geometry::Translation'>Translation</a>&lt;<a class='struct' href='http://nalgebra.org/doc/nalgebra/structs/vec/struct.Vec3.html' title='nalgebra::structs::vec::Vec3'>Vec3</a>&lt;<a href='http://doc.rust-lang.org/std/primitive.f32.html'>f32</a>&gt;&gt; for <a class='struct' href='kiss3d/camera/struct.FirstPerson.html' title='kiss3d::camera::FirstPerson'>FirstPerson</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
