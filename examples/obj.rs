c10b419bed1598b819052017ee2ca64229952bc3
Sébastien Crozet <developer@crozet.re>
2014-08-17 22:47:36 +0200

    Add the rust logo to the `obj.rs` demo.

--- examples/obj.rs
+++ examples/obj.rs
@@ -13,13 +13,24 @@ fn start(argc: int, argv: *const *const u8) -> int {
 
 fn main() {
     let mut window = Window::new("Kiss3d: obj");
+
+    // Teapot
     let obj_path   = Path::new("media/teapot/teapot.obj");
     let mtl_path   = Path::new("media/teapot");
-    let mut o      = window.add_obj(&obj_path, &mtl_path, Vec3::new(0.001, 0.001, 0.001));
+    let mut teapot = window.add_obj(&obj_path, &mtl_path, Vec3::new(0.001, 0.001, 0.001));
+    teapot.append_translation(&Vec3::new(0.0, -0.05, -0.2));
+
+    // Rust logo
+    let obj_path = Path::new("media/rust_logo/rust_logo.obj");
+    let mtl_path = Path::new("media/rust_logo");
+    let mut rust = window.add_obj(&obj_path, &mtl_path, Vec3::new(0.05, 0.05, 0.05));
+    rust.prepend_to_local_rotation(&Vec3::new(-1.0 * Float::frac_pi_2(), 0.0, 0.0));
+    rust.set_color(0.0, 0.0, 1.0);
 
     window.set_light(light::StickToCamera);
 
     while window.render() {
-        o.prepend_to_local_rotation(&Vec3::new(0.0f32, 0.014, 0.0));
+        teapot.prepend_to_local_rotation(&Vec3::new(0.0f32, 0.014, 0.0));
+        rust.prepend_to_local_rotation(&Vec3::new(0.0f32, -0.014, 0.0));
     }
 }
