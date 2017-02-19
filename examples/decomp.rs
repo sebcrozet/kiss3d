510665236c9de59e636b41a4f5e5c4a9c34c52e2
Sébastien Crozet <developer@crozet.re>
2017-02-19 17:23:39 +0100

    Release v0.8.0.

---
+++ examples/decomp.rs
@@ -0,0 +1,131 @@
+extern crate rand;
+extern crate ncollide_transformation;
+extern crate kiss3d;
+extern crate nalgebra as na;
+
+use std::str::FromStr;
+use std::env;
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::sync::{Arc, RwLock};
+use std::path::Path;
+use std::time::Instant;
+use rand::random;
+use na::{Translation3, Vector3};
+use kiss3d::window::Window;
+use kiss3d::light::Light;
+use kiss3d::loader::obj;
+use kiss3d::resource::{BufferType, AllocationType, GPUVec, Mesh};
+
+fn usage(exe_name: &str) {
+    println!("Usage: {} obj_file scale clusters concavity", exe_name);
+    println!("");
+    println!("Options:");
+    println!("    obj_file  - the obj file to decompose.");
+    println!("    scale     - the scale to apply to the displayed model.");
+    println!("    clusters  - minimum number of clusters.");
+    println!("    concavity - the maximum concavity accepted by the decomposition.");
+}
+
+fn main() {
+    /*
+     * Parse arguments.
+     */
+    let mut args = env::args();
+    let exname = args.next().unwrap();
+
+    if args.len() != 4 {
+        usage(&exname[..]);
+        return;
+    }
+
+    let path            = &args.next().unwrap()[..];
+    let scale: f32      = FromStr::from_str(&args.next().unwrap()[..]).unwrap();
+    let clusters: usize = FromStr::from_str(&args.next().unwrap()[..]).unwrap();
+    let concavity: f32  = FromStr::from_str(&args.next().unwrap()[..]).unwrap();
+
+    let scale = Vector3::from_element(scale);
+
+    /*
+     * Create the window.
+     */
+    let mut window = Window::new("Kiss3d: convex decomposition");
+
+    /*
+     * Convex decomposition.
+     */
+    let obj_path = Path::new(path);
+    // let obj_path = Path::new("/home/tortue/Downloads/models/ATST_medium.obj");
+    let mtl_path = Path::new("none");
+    let teapot   = obj::parse_file(&obj_path, &mtl_path, "none").unwrap();
+
+    let mut m = window.add_obj(&obj_path, &mtl_path, scale);
+    m.set_surface_rendering_activation(false);
+    // m.set_lines_width(1.0);
+    let data    = m.data();
+    let coords  = data.object().expect("here").mesh().borrow().coords().clone();
+    let normals = data.object().unwrap().mesh().borrow().normals().clone();
+    let uvs     = data.object().unwrap().mesh().borrow().uvs().clone();
+    let faces   = data.object().unwrap().mesh().borrow().faces().clone();
+
+    // println!("objs: {}", teapot.len());
+
+    let mut total_time = 0.0f64;
+    for &(_, ref mesh, _) in teapot.iter() {
+        match mesh.to_trimesh() {
+            Some(mut trimesh) => {
+                trimesh.split_index_buffer(true);
+                let begin = Instant::now();
+                let (decomp, partitioning) = ncollide_transformation::hacd(trimesh, concavity, clusters);
+                let elapsed = begin.elapsed();
+                total_time = elapsed.as_secs() as f64 + elapsed.subsec_nanos() as f64 / 1000000000.0;
+
+                println!("num comps: {}", decomp.len());
+
+                for (comp, partitioning) in decomp.into_iter().zip(partitioning.into_iter()) {
+                    let r = random();
+                    let g = random();
+                    let b = random();
+
+                    let mut m  = window.add_trimesh(comp, scale);
+                    m.set_color(r, g, b);
+                    m.append_translation(&Translation3::new(-0.1, 0.1, 0.0));
+                    // m.set_surface_rendering_activation(false);
+                    // m.enable_backface_culling(false);
+                    // m.set_lines_width(1.0);
+
+                    let mut part_faces = Vec::new();
+
+                    for i in partitioning.into_iter() {
+                        part_faces.push(faces.read().unwrap().data().as_ref().unwrap()[i]);
+                    }
+
+                    let faces = GPUVec::new(part_faces, BufferType::ElementArray, AllocationType::StaticDraw);
+                    let faces = Arc::new(RwLock::new(faces));
+
+                    let mesh = Mesh::new_with_gpu_vectors(coords.clone(), faces, normals.clone(), uvs.clone());
+                    let mesh = Rc::new(RefCell::new(mesh));
+                    let mut m  = window.add_mesh(mesh, scale);
+                    m.set_color(r, g, b);
+                    m.append_translation(&Translation3::new(0.1, 0.1, 0.0));
+                    // m.set_surface_rendering_activation(false);
+                    // m.enable_backface_culling(false);
+                    m.set_lines_width(1.0);
+                }
+            },
+            None => { }
+        }
+    }
+
+    println!("Decomposition time: {}", total_time);
+
+    /*
+     *
+     * Rendering.
+     *
+     */
+    window.set_light(Light::StickToCamera);
+
+    while window.render() {
+    }
+}
---
+++ examples/procedural.rs
@@ -0,0 +1,172 @@
+extern crate rand;
+extern crate ncollide_procedural;
+extern crate ncollide_transformation;
+extern crate kiss3d;
+extern crate nalgebra as na;
+
+use std::path::Path;
+use na::{Translation3, Point2, Point3, Vector2, Vector3};
+use ncollide_procedural::{Polyline, TriMesh};
+use ncollide_procedural::path::{PolylinePath, PolylinePattern, StrokePattern, ArrowheadCap};
+use kiss3d::window::Window;
+use kiss3d::light::Light;
+
+fn main() {
+    let mut window = Window::new("Kiss3d: procedural");
+
+    /*
+     * A cube.
+     */
+    let cube  = ncollide_procedural::cuboid(&Vector3::new(0.7f32, 0.2, 0.4));
+    let mut c = window.add_trimesh(cube, Vector3::from_element(1.0));
+    c.append_translation(&Translation3::new(1.0, 0.0, 0.0));
+    c.set_texture_from_file(&Path::new("media/kitten.png"), "kitten");
+
+    /*
+     * A sphere.
+     */
+    let sphere = ncollide_procedural::sphere(0.4f32, 20, 20, true);
+    let mut s  = window.add_trimesh(sphere, Vector3::from_element(1.0));
+    s.set_texture_with_name("kitten");
+
+    /*
+     * A capsule.
+     */
+    let capsule = ncollide_procedural::capsule(&0.4f32, &0.4f32, 20, 20);
+    let mut c   = window.add_trimesh(capsule, Vector3::from_element(1.0));
+    c.append_translation(&Translation3::new(-1.0, 0.0, 0.0));
+    c.set_color(0.0, 0.0, 1.0);
+
+    /*
+     * Triangulation.
+     */
+    let to_triangulate = ncollide_transformation::triangulate(&[
+        Point3::new(5.0f32, 0.0, 0.0), Point3::new(6.1, 0.0, 0.5), Point3::new(7.4, 0.0, 0.5), Point3::new(8.2, 0.0, 0.0),
+        Point3::new(5.1f32, 1.0, 0.0), Point3::new(6.2, 1.5, 0.5), Point3::new(7.2, 1.0, 0.5), Point3::new(8.0, 1.3, 0.0),
+        Point3::new(5.3f32, 2.0, 0.0), Point3::new(6.1, 2.2, 0.5), Point3::new(7.3, 2.0, 0.5), Point3::new(8.2, 2.4, 0.0),
+        Point3::new(5.2f32, 3.0, 0.0), Point3::new(6.1, 2.9, 0.5), Point3::new(7.4, 3.0, 0.5), Point3::new(8.0, 3.1, 0.0)
+    ]);
+    let mut t = window.add_trimesh(to_triangulate, Vector3::from_element(1.0));
+    t.set_surface_rendering_activation(false);
+    t.set_lines_width(2.0);
+    t.set_color(0.0, 1.0, 0.0);
+
+    /*
+     * A (non-rational) bicubic Bézier surface.
+     */
+    let control_points = [
+        Point3::new(0.0f32, 0.0, 0.0), Point3::new(1.0, 0.0, 2.0), Point3::new(2.0, 0.0, 2.0), Point3::new(3.0, 0.0, 0.0),
+        Point3::new(0.0f32, 1.0, 2.0), Point3::new(1.0, 1.0, 3.0), Point3::new(2.0, 1.0, 3.0), Point3::new(3.0, 1.0, 2.0),
+        Point3::new(0.0f32, 2.0, 2.0), Point3::new(1.0, 2.0, 3.0), Point3::new(2.0, 2.0, 3.0), Point3::new(3.0, 2.0, 2.0),
+        Point3::new(0.0f32, 3.0, 0.0), Point3::new(1.0, 3.0, 2.0), Point3::new(2.0, 3.0, 2.0), Point3::new(3.0, 3.0, 0.0)
+    ];
+    let bezier = ncollide_procedural::bezier_surface(&control_points, 4, 4, 100, 100);
+    let mut b  = window.add_trimesh(bezier, Vector3::from_element(1.0));
+    b.append_translation(&Translation3::new(-1.5, -1.5, 0.0));
+    b.enable_backface_culling(false);
+
+    // XXX: replace by an `add_mesh`.
+    let mut control_polyhedra_gfx = window.add_quad_with_vertices(&control_points, 4, 4);
+    control_polyhedra_gfx.append_translation(&Translation3::new(-1.5, -1.5, 0.0));
+    control_polyhedra_gfx.set_color(0.0, 0.0, 1.0);
+    control_polyhedra_gfx.set_surface_rendering_activation(false);
+    control_polyhedra_gfx.set_lines_width(2.0);
+
+    let mut control_points_gfx = window.add_mesh(control_polyhedra_gfx.data().get_object().mesh().clone(), Vector3::from_element(1.0));
+    control_points_gfx.append_translation(&Translation3::new(-1.5, -1.5, 0.0));
+    control_points_gfx.set_color(1.0, 0.0, 0.0);
+    control_points_gfx.set_surface_rendering_activation(false);
+    control_points_gfx.set_points_size(10.0);
+
+    /*
+     * Path stroke.
+     */
+    let control_points = [
+        Point3::new(0.0f32, 1.0, 0.0),
+        Point3::new(2.0f32, 4.0, 2.0),
+        Point3::new(2.0f32, 1.0, 4.0),
+        Point3::new(4.0f32, 4.0, 6.0),
+        Point3::new(2.0f32, 1.0, 8.0),
+        Point3::new(2.0f32, 4.0, 10.0),
+        Point3::new(0.0f32, 1.0, 12.0),
+        Point3::new(-2.0f32, 4.0, 10.0),
+        Point3::new(-2.0f32, 1.0, 8.0),
+        Point3::new(-4.0f32, 4.0, 6.0),
+        Point3::new(-2.0f32, 1.0, 4.0),
+        Point3::new(-2.0f32, 4.0, 2.0),
+    ];
+    let bezier      = ncollide_procedural::bezier_curve(&control_points, 100);
+    let mut path    = PolylinePath::new(&bezier);
+    let pattern     = ncollide_procedural::unit_circle(100);
+    let start_cap   = ArrowheadCap::new(1.5f32, 2.0, 0.0);
+    let end_cap     = ArrowheadCap::new(2.0f32, 2.0, 0.5);
+    let mut pattern = PolylinePattern::new(&pattern, true, start_cap, end_cap);
+    let mesh        = pattern.stroke(&mut path);
+    let mut m       = window.add_trimesh(mesh, Vector3::new(0.5f32, 0.5, 0.5));
+    m.append_translation(&Translation3::new(4.0, -1.0, 0.0));
+    m.set_color(1.0, 1.0, 0.0);
+
+    /*
+     * Convex hull of 100,000 random 3d points.
+     */
+    let mut points = Vec::new();
+    for _ in 0usize .. 100000 {
+        points.push(rand::random::<Point3<f32>>() * 2.0f32);
+    }
+
+    let chull  = ncollide_transformation::convex_hull3(&points[..]);
+    let mut mhull = window.add_trimesh(chull, Vector3::from_element(1.0));
+    let mut mpts  = window.add_trimesh(TriMesh::new(points, None, None, None), Vector3::from_element(1.0));
+    mhull.append_translation(&Translation3::new(0.0, 2.0, -1.0));
+    mhull.set_color(0.0, 1.0, 0.0);
+    mhull.set_lines_width(2.0);
+    mhull.set_surface_rendering_activation(false);
+    mhull.set_points_size(10.0);
+    mpts.set_color(0.0, 0.0, 1.0);
+    mpts.append_translation(&Translation3::new(0.0, 2.0, -1.0));
+    mpts.set_points_size(2.0);
+    mpts.set_surface_rendering_activation(false);
+
+    /*
+     * Convex hull of 100,000 random 2d points.
+     */
+    let mut points = Vec::new();
+    let origin     = Point2::new(3.0f32, 2.0);
+    for _ in 0usize .. 100000 {
+        points.push(origin + rand::random::<Vector2<f32>>() * 2.0f32);
+    }
+
+    let points   = &points[..];
+    let polyline = ncollide_transformation::convex_hull2(points);
+
+    /*
+     *
+     * Rendering.
+     *
+     */
+    window.set_light(Light::StickToCamera);
+
+    while window.render() {
+        draw_polyline(&mut window, &polyline, points);
+    }
+}
+
+fn draw_polyline(window: &mut Window, polyline: &Polyline<Point2<f32>>, points: &[Point2<f32>]) {
+    for pt in polyline.coords().windows(2) {
+        window.draw_line(&Point3::new(pt[0].x, pt[0].y, 0.0), &Point3::new(pt[1].x, pt[1].y, 0.0), &Point3::new(0.0, 1.0, 0.0));
+    }
+
+    let last = polyline.coords().len() - 1;
+    window.draw_line(&Point3::new(polyline.coords()[0].x, polyline.coords()[0].y, 0.0),
+                     &Point3::new(polyline.coords()[last].x, polyline.coords()[last].y, 0.0),
+                     &Point3::new(0.0, 1.0, 0.0));
+
+    for pt in points.iter() {
+            window.draw_point(&Point3::new(pt.x, pt.y, 0.0), &Point3::new(0.0, 0.0, 1.0));
+    }
+
+    for pt in polyline.coords().iter() {
+        window.draw_point(&Point3::new(pt.x, pt.y, 0.0), &Point3::new(1.0, 0.0, 0.0));
+    }
+
+}
