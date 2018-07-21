9ef7aa6c3f566f2972a48890d4bff024a0c0b664
sebcrozet <developer@crozet.re>
2018-07-21 14:10:26 +0200

    CI: don't compile for wasm32-unknown-asmjs because that requires emscripten.

--- ci.sh
+++ ci.sh
@@ -7,7 +7,5 @@ apt upgrade -y
 apt install -y clang cmake build-essential libxxf86vm-dev libxrandr-dev xorg-dev libglu1-mesa-dev libxrandr2 libglfw3 libglfw3-dev # libglfw3-wayland
 
 rustup target add wasm32-unknown-unknown
-rustup target add wasm32-unknown-asmjs
 cargo test
 cargo build --target wasm32-unknown-unknown
-cargo build --target wasm32-unknown-asmjs
