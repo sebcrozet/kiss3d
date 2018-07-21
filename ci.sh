a19eb9c81ee89d649a3cfa89dbd406aebf1badda
sebcrozet <developer@crozet.re>
2018-07-21 13:52:03 +0200

    Add CI build for wasm32 platform.

--- ci.sh
+++ ci.sh
@@ -7,3 +7,5 @@ apt upgrade -y
 apt install -y clang cmake build-essential libxxf86vm-dev libxrandr-dev xorg-dev libglu1-mesa-dev libxrandr2 libglfw3 libglfw3-dev # libglfw3-wayland
 
 cargo test
+cargo build --target wasm32-unknown-unknown
+cargo build --target wasm32-unknown-asmjs
