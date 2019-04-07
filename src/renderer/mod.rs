835ef3a379d597fa98561743a7b6f1777bad811f
sebcrozet <developer@crozet.re>
2019-04-07 11:17:50 +0200

    Add missing mod.rs

---
+++ src/renderer/mod.rs
@@ -0,0 +1,10 @@
+//! Structures responsible for rendering elements other than kiss3d's meshes.
+
+pub use self::point_renderer::PointRenderer;
+pub use self::line_renderer::LineRenderer;
+pub use self::renderer::Renderer;
+
+
+pub mod line_renderer;
+pub mod point_renderer;
+mod renderer;
