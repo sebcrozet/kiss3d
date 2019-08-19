6822a553795b90ffab30f5c8416babfcc28511b4
Slyklaw Dev <slyklaw@gmail.com>
2019-08-19 20:12:33 +0700

    Move shaders to individual files.

--- src/builtin/object_material.rs
+++ src/builtin/object_material.rs
@@ -181,63 +181,8 @@ pub static OBJECT_VERTEX_SRC: &'static str = A_VERY_LONG_STRING;
 /// Fragment shader of the default object material.
 pub static OBJECT_FRAGMENT_SRC: &'static str = ANOTHER_VERY_LONG_STRING;
 
-const A_VERY_LONG_STRING: &'static str = "#version 100
-attribute vec3 position;
-attribute vec2 tex_coord;
-attribute vec3 normal;
-
-uniform mat3 ntransform, scale;
-uniform mat4 proj, view, transform;
-uniform vec3 light_position;
-
-varying vec3 local_light_position;
-varying vec2 tex_coord_v;
-varying vec3 normalInterp;
-varying vec3 vertPos;
-
-void main(){
-    gl_Position = proj * view * transform * vec4(scale * position, 1.0);
-    vec4 vertPos4 = view * transform * vec4(scale * position, 1.0);
-    vertPos = vec3(vertPos4) / vertPos4.w;
-    normalInterp = mat3(view) * ntransform * normal;
-    tex_coord_v = tex_coord;
-    local_light_position = (view * vec4(light_position, 1.0)).xyz;
-}";
+const A_VERY_LONG_STRING: &'static str = include_str!("default.vert");
 
 // phong-like lighting (heavily) inspired
 // http://www.mathematik.uni-marburg.de/~thormae/lectures/graphics1/code/WebGLShaderLightMat/ShaderLightMat.html
-const ANOTHER_VERY_LONG_STRING: &'static str = "#version 100
-#ifdef GL_FRAGMENT_PRECISION_HIGH
-   precision highp float;
-#else
-   precision mediump float;
-#endif
-
-varying vec3 local_light_position;
-varying vec2 tex_coord_v;
-varying vec3 normalInterp;
-varying vec3 vertPos;
-
-uniform vec3 color;
-uniform sampler2D tex;
-const vec3 specColor = vec3(0.4, 0.4, 0.4);
-
-void main() {
-  vec3 normal = normalize(normalInterp);
-  vec3 lightDir = normalize(local_light_position - vertPos);
-
-  float lambertian = max(dot(lightDir, normal), 0.0);
-  float specular = 0.0;
-
-  if(lambertian > 0.0) {
-    vec3 viewDir = normalize(-vertPos);
-    vec3 halfDir = normalize(lightDir + viewDir);
-    float specAngle = max(dot(halfDir, normal), 0.0);
-    specular = pow(specAngle, 30.0);
-  }
-
-  vec4 tex_color = texture2D(tex, tex_coord_v);
-  gl_FragColor = tex_color * vec4(color / 3.0 +
-                                  lambertian * color / 3.0 +
-                                  specular * specColor / 3.0, 1.0);
-}";
+const ANOTHER_VERY_LONG_STRING: &'static str = include_str!("default.frag");
