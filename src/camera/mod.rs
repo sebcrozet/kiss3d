cd4f8f77a493512558d7c2b772b6efdc215ae38f
Aslan <100476245+Xtry15@users.noreply.github.com>
2022-10-24 15:33:15 +0600

    Update mod.rs

--- src/camera/mod.rs
+++ src/camera/mod.rs
@@ -1,12 +1,10 @@
 //! Camera trait with some common implementations.
 
-pub use self::arc_ball::ArcBall;
 pub use self::camera::Camera;
 pub use self::first_person::FirstPerson;
 pub use self::first_person_stereo::FirstPersonStereo;
 pub use self::fixed_view::FixedView;
 
-mod arc_ball;
 #[doc(hidden)]
 pub mod camera;
 mod first_person;
